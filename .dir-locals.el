d45cd45afa2cc94869b46c7fc561e734b24e9db0
Rafał Cieślak <rafalcieslak256@gmail.com>
2016-05-26 15:43:56 +0200

    Automatically generated .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,15 +1,12 @@
-(
- (nil . (
-         (flycheck-clang-language-standard "c++11")
-         )
-      )
- (c++-mode . (
-              (eval . (progn
-                        (setq flycheck-clang-include-path (list (expand-file-name "./external/include/")))
-                        (flycheck-select-checker 'c/c++-clang)
-                       )
-                    )
-              )
-           )
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
 
- )
+((nil)
+ (c++-mode
+  (flycheck-gcc-language-standard . "c++11")
+  (flycheck-clang-language-standard . "c++11")
+  (eval progn
+        (setq flycheck-clang-include-path
+              (list
+               (expand-file-name "./external/include/")))
+        (flycheck-select-checker 'c/c++-clang))))
