7a0958795e39e73033dacda73bbf9fd83f5ded0b
Rafał Cieślak <rafalcieslak256@gmail.com>
2016-05-26 15:19:32 +0200

    Using c++11 in dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,13 @@
 (
+ (nil . (
+         (flycheck-clang-language-standard "c++11")
+         )
+      )
  (c++-mode . (
               (eval . (progn
                         (setq flycheck-clang-include-path (list (expand-file-name "./external/include/")))
                         (flycheck-select-checker 'c/c++-clang)
-                        )
+                       )
                     )
               )
            )
